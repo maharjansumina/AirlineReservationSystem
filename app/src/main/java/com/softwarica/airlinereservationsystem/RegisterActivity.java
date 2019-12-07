91c37dd274ae7492f6f40cd8e0307a3070e66490
sumina <suminamaharjan098@gmail.com>
2019-12-07 09:14:57 +0545

    Design to the Register Activity is Added

--- app/src/main/java/com/softwarica/airlinereservationsystem/RegisterActivity.java
+++ app/src/main/java/com/softwarica/airlinereservationsystem/RegisterActivity.java
@@ -3,12 +3,25 @@ package com.softwarica.airlinereservationsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText etName, etAddress, etPhone, etEmail, etPassword;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Blinding
+        etName = findViewById(R.id.etName);
+        etAddress = findViewById(R.id.etAddress);
+        etPhone = findViewById(R.id.etPhone);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
     }
 }
