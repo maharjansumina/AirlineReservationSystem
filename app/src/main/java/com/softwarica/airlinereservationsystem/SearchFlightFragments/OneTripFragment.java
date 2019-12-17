12dfcab1026a29b429f570d3ffa1c5f868a0d5a5
sumina <suminamaharjan098@gmail.com>
2019-12-17 20:46:31 +0545

    Design to the One and Round Way Trip is Added using Fragments.

--- app/src/main/java/com/softwarica/airlinereservationsystem/SearchFlightFragments/OneTripFragment.java
+++ app/src/main/java/com/softwarica/airlinereservationsystem/SearchFlightFragments/OneTripFragment.java
@@ -1,6 +1,7 @@
 package com.softwarica.airlinereservationsystem.SearchFlightFragments;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.softwarica.airlinereservationsystem.R;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class OneTripFragment extends Fragment {
+public class OneTripFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    private Spinner spinFrom, spinTo, spinNationality;
+    private EditText etDeparture, etTravellers;
 
 
     public OneTripFragment() {
@@ -26,7 +36,65 @@ public class OneTripFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one_trip, container, false);
+        View view = inflater.inflate(R.layout.fragment_one_trip, container, false);
+
+        spinFrom = view.findViewById(R.id.spinFrom);
+        spinTo = view.findViewById(R.id.spinTo);
+        spinNationality = view.findViewById(R.id.spinNationality);
+        etDeparture = view.findViewById(R.id.etDeparture);
+        etTravellers = view.findViewById(R.id.etTravellers);
+
+        String From [] = {"Kathmandu" , "Pokhara", "Janakpur" , "Nepalgunj", "Birtnagar"};
+        final ArrayAdapter from = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                From
+        );
+        spinFrom.setAdapter(from);
+//
+        String To [] = {"Pokhara" , "Janakpur", "Kathmandu", "Nepalgunj", "Birtnagar"};
+        final ArrayAdapter to = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                To
+        );
+        spinTo.setAdapter(to);
+
+        String Nationality [] = {"Nepal"};
+        final ArrayAdapter nationality = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                Nationality
+        );
+        spinNationality.setAdapter(nationality);
+
+        etDeparture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDeparture();
+            }
+        });
+
+        return view;
     }
 
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String date =  (month + 1) + "/" + dayOfMonth + "/" + year;
+        etDeparture.setText(date);
+    }
+
+    private void loadDeparture(){
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+//        DatePickerDialog datePickerDialog = new DatePickerDialog(
+//                getActivity().getApplicationContext()
+//                year, month, day);
+//        datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+//        datePickerDialog.show();
+    }
 }
--- app/src/main/java/com/softwarica/airlinereservationsystem/SearchFlightFragments/RoundTripFragment.java
+++ app/src/main/java/com/softwarica/airlinereservationsystem/SearchFlightFragments/RoundTripFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.softwarica.airlinereservationsystem.R;
 
@@ -15,6 +18,8 @@ import com.softwarica.airlinereservationsystem.R;
  * A simple {@link Fragment} subclass.
  */
 public class RoundTripFragment extends Fragment {
+    private Spinner spinFrom, spinTo, spinNationality;
+    private EditText etDeparture, etTravellers;
 
 
     public RoundTripFragment() {
@@ -26,7 +31,39 @@ public class RoundTripFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_round_trip, container, false);
+        View view = inflater.inflate(R.layout.fragment_round_trip, container, false);
+
+        spinFrom = view.findViewById(R.id.spinFrom);
+        spinTo = view.findViewById(R.id.spinTo);
+        spinNationality = view.findViewById(R.id.spinNationality);
+        etDeparture = view.findViewById(R.id.etDeparture);
+        etTravellers = view.findViewById(R.id.etTravellers);
+
+        String From [] = {"Kathmandu" , "Pokhara", "Janakpur" , "Nepalgunj", "Birtnagar"};
+        final ArrayAdapter from = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                From
+        );
+        spinFrom.setAdapter(from);
+//
+        String To [] = {"Pokhara" , "Janakpur", "Kathmandu", "Nepalgunj", "Birtnagar"};
+        final ArrayAdapter to = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                To
+        );
+        spinTo.setAdapter(to);
+
+        String Nationality [] = {"Nepal"};
+        final ArrayAdapter nationality = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                Nationality
+        );
+        spinNationality.setAdapter(nationality);
+
+        return view;
     }
 
 }
