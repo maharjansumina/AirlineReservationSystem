7b30d4a2a7495a91a67425198ca6c7b4392808b1
maharjansumina <suminamaharjan098@gmail.com>
2020-01-31 12:16:03 +0545

    Admin Dashboard Layout Added.

--- app/src/main/java/com/softwarica/airlinereservationsystem/Admin/AdminDashboardActivity.java
+++ app/src/main/java/com/softwarica/airlinereservationsystem/Admin/AdminDashboardActivity.java
@@ -1,16 +1,39 @@
 package com.softwarica.airlinereservationsystem.Admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.softwarica.airlinereservationsystem.BookingDetail.SearchFlightActivity;
+import com.softwarica.airlinereservationsystem.ContactUsActivity;
 import com.softwarica.airlinereservationsystem.R;
 
-public class AdminDashboardActivity extends AppCompatActivity {
+public class AdminDashboardActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView cvViewUser, cvAdminProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+
+        cvAdminProfile = findViewById(R.id.cvAdminProfile);
+        cvViewUser = findViewById(R.id.cvCancel);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cvSearchFlight:
+                startActivity(new Intent(this, SearchFlightActivity.class));
+                break;
+
+            case R.id.cvContactUs:
+                startActivity(new Intent(this, ContactUsActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/softwarica/airlinereservationsystem/Admin/ViewUserdetailActivity.java
@@ -0,0 +1,16 @@
+package com.softwarica.airlinereservationsystem.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.softwarica.airlinereservationsystem.R;
+
+public class ViewUserdetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_userdetail);
+    }
+}
